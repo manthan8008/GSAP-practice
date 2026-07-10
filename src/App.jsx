c95592868c0f824d77135ec4ee640188bf5fcb9e
manthan8008 <jainn.manthann@gmail.com>
2026-07-10 12:48:59 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,26 @@ gsap.registerPlugin(ScrollTrigger);
 export default function App() {
   const containerRef = useRef(null);
 
-  useGSAP(() => {
-    const boxes = gsap.utils.toArray(containerRef.current.children);
-    boxes.map((box) => {
-      gsap.to(box, {
-        x: 400,
-        borderRadius: "50%",
-        rotation: 360,
-        ease: "power1.inOut",
-        scrollTrigger: {
-          start: "bottom bottom",
-          end: "top 20%",
-          trigger: box,
-          scrub: 0.5,
-        },
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray(containerRef.current.children);
+      boxes.map((box) => {
+        gsap.to(box, {
+          x: 400,
+          borderRadius: "50%",
+          rotation: 360,
+          ease: "power1.inOut",
+          scrollTrigger: {
+            start: "bottom bottom",
+            end: "top 20%",
+            trigger: box,
+            scrub: 0.5,
+          },
+        });
       });
-    });
-  }, []);
+    },
+    { scope: containerRef },
+  );
 
   return (
     <div ref={containerRef} className="my-[150%] flex flex-col gap-10">
